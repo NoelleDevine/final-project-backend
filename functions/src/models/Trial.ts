0d08e126eb193002a4aceb58afdaf4e7f7c3cd7a
Anyssa <patelany05@gmail.com>
2023-06-16 16:20:16 -0400

    friday 6/16

--- functions/src/models/Trial.ts
+++ functions/src/models/Trial.ts
@@ -3,13 +3,14 @@ import Reaction from "./Reaction";
 
 export default interface Trial {
   _id?: ObjectId;
-  patient_id: ObjectId;
+  guardianID: string;
+  patient_id: string;
   trial_name: string;
   trial_type: string;
   food_type: string;
   trial_food: string;
-  food_photo_url: string;
+  food_photo_url?: string;
   start_date: Date; //current date and time
-  trial_pass: boolean;
-  reaction: Reaction[];
+  trial_pass: string;
+  reaction?: Reaction[];
 }
